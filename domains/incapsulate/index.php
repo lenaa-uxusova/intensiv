0861a81b5f118acf78f73ab195cb6dbf85483647
Елена <uxusova.lena@gmail.com>
2023-10-21 19:19:47 +0300

    20,10

---
+++ domains/incapsulate/index.php
@@ -0,0 +1,21 @@
+<?php
+   class Car
+ {
+   $name='';
+   public $color='';
+
+   public function start()
+   {
+     echo '1,2,3... Start!';
+   }
+
+   public function stop()
+   {
+      echo 'Stop!';
+   }
+  public function
+ }
+   $car1 = new Car();
+   echo $car1 ->name;
+   echo $car1 -> start();
+   ?>
